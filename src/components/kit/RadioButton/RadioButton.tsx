4858f578809ae3b5ad1ced61ab3bf42102dddb07
m.sharghi <sharghi.mahtab@gmail.com>
2024-11-02 14:20:11 +0330

    fix: fix disable radio in dark moe

--- src/components/kit/RadioButton/RadioButton.tsx
+++ src/components/kit/RadioButton/RadioButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Radio, useTheme, Box, FormControlLabel } from "@mui/material";
+import { Radio, Box, FormControlLabel, useTheme } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 interface IRadioButtonKitProps {
@@ -12,29 +12,36 @@ interface IRadioButtonKitProps {
   disabled?: boolean;
 }
 
-const StyledRadioButton = styled(Radio)(({ theme, color }) => ({
-  "&:hover": {
-    backgroundColor:
-      color === "primary" ? theme.palette.primary[95] : theme.palette.error[95],
-  },
-  "&:focused": {
-    backgroundColor:
-      color === "primary" ? theme.palette.primary[90] : theme.palette.error[90],
-  },
-  "&:pressed": {
-    backgroundColor: theme.palette.primary[95],
-  },
-}));
+const StyledRadioButton = styled(Radio)(({ theme, color, disabled }) => ({
+    "&.Mui-disabled": {
+        color: theme.palette.mode === "dark"
+      ? theme.palette.grey[50]
+      : theme.palette.grey[400]
+    },
+    "&:hover": {
+      backgroundColor:
+        color === "primary" ? theme.palette.primary[95] : theme.palette.error[95],
+    },
+    "&:focused": {
+      backgroundColor:
+        color === "primary" ? theme.palette.primary[90] : theme.palette.error[90],
+    },
+    "&:pressed": {
+      backgroundColor: theme.palette.primary[95],
+    },
+  }));
+
 
 const RadioButtonKit: React.FC<IRadioButtonKitProps> = (props) => {
-  const theme = useTheme();
-  const { label1, label2, color = "primary", size, disabled } = props;
+//   const theme = useTheme();
+  const { label1, label2, color, size, disabled } = props;
   const [selectedValue, setSelectedValue] = React.useState("a");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedValue(event.target.value);
   };
 
+  
   return (
     <Box>
       <FormControlLabel
